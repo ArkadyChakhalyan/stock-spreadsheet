e6ec690d3dc1bc0abf608b71b8913a6bee4fb339
Armen Chakhalyan <mench256@gmail.com>
2021-07-18 14:32:23 +0300

    first real commit  - added react-app  - createdd ui button

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './ui/button/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>
+        First Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
