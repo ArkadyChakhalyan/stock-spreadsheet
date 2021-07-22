b3fac117cf500f4c7ddc7a8afe87730d32178d8e
Arkady Chakhalyan <arkadychakhalyan@gmail.com>
2021-07-22 17:49:56 +0200

    commit tabs ui text ui heading ui container ui some minor updates

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,26 +2,44 @@ import React from 'react';
 import Button from '../../ui/button/';
 import Input from '../../ui/input';
 import StockMiniature from '../../ui/stock-miniature';
+import Tab from '../../ui/tab';
 import './app.css';
 
 const App = () => {
 
   return (
     <div className='App'>
+      <div className='container'>
+      <h1>Heading</h1>
+      <h2>Subtitle</h2>
+      <p>Text example Text example Text example Text example</p>
+      <div className='main-menu'>
+      <Tab options={['stocks', 'dividends', 'sectors']}/>
+      </div>
+      <br /><br />
+      </div>
+      
+      <Tab options={['monthly', 'yearly']} type={'small'} />
+      <br /><br />
       <Button icon = {'fas fa-plus fa-sm'} width={250} color={'#007000'}>
-        NEW BUY
+        new buy
       </Button>
       <br /><br />
       <Button icon = {'fas fa-pen fa-sm'}>
-        EDIT
+        edit
       </Button>
       <br /><br />
       <Button icon = {'fas fa-trash'} color={'#D2222D'}>
-        DELETE
+        delete
       </Button>
       <br /><br />
       <Button type={'round'} icon = {'fas fa-times'} color={'#D2222D'}/>
+      <br /><br />
+      <Button type={'round'} icon = {'fas fa-chevron-left'}/>
+      <Button type={'round'} icon = {'fas fa-chevron-right'}/>
+      <br /><br />
       <Input label={'ticker'} />
+      <br />
       <Input label={'price'} />
       <StockMiniature gain={10}/>
       <StockMiniature gain={-5}/>
---
+++ src/ui/tab/tab.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './tab.css';
+
+const Tab = ({ options, type }) => {
+
+    const onClick = (e) => {
+        if (active) active.className = className;
+        active = e.target;
+        // setLeft(active.id * 124);
+        // setLeft(active.id * 86);
+
+        active.className += 'active';
+    };
+
+    // как сделать первый элемент сразу активным
+    // как передвигать слайдер, юз стейт теряет переменные
+
+    let active;
+    let className = 'tab single ';
+    let barClassName = 'bar ';
+    let id = -1;
+    let left
+    // const [left, setLeft] = useState(0);
+    if (type) {
+        className += 'small ';
+        barClassName += 'small';
+    }
+
+    const elements = options.map((item) => {
+        id++
+        return (
+            <div
+                className={className}
+                id={id}
+                onClick={onClick}>
+                {item}
+            </div>
+        )
+    });
+
+    return (
+        <div className='tab' id='menu'>
+            {elements}
+            <span className={barClassName} style={{ left: `${left}px` }} />
+        </div>
+    );
+};
+
+export default Tab;
